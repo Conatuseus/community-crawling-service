68ba1969a6e4d4673b737d442ece197078573a9b
MK S <conatuseus@gmail.com>
2020-01-10 17:35:38 +0900

    Project Initialize

---
+++ src/main/java/com/conatuseus/communityservice/CommunityCrawlingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.conatuseus.communityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityCrawlingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityCrawlingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/conatuseus/communityservice/communitycrawlingservice/CommunityCrawlingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conatuseus.communityservice.communitycrawlingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityCrawlingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
