3d8b3812f8f7a18504ea1d2b48b1ff5c7cdbabec
사명기 <conatuseus@gmail.com>
2020-01-12 21:41:29 +0900

    Feature #10 커뮤니티 UPDATE API 구현 (#18)
    * feature: community Update를 위한 DTO 구현
    
    * feature: Community Update 메서드 구현
    
    * feature: update 로직 구현
    
    * feature: Community Update 테스트 구현(Controller, Service, Repository)

---
+++ src/main/java/com/conatuseus/communityservice/domain/community/service/dto/CommunityUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.conatuseus.communityservice.domain.community.service.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CommunityUpdateRequestDto {
+
+    private String name;
+    private String searchUrl;
+    private String keyword;
+    private String cssQuery;
+    private String attributeKey;
+
+    @Builder
+    public CommunityUpdateRequestDto(final String name, final String searchUrl, final String keyword, final String cssQuery, final String attributeKey) {
+        this.name = name;
+        this.searchUrl = searchUrl;
+        this.keyword = keyword;
+        this.cssQuery = cssQuery;
+        this.attributeKey = attributeKey;
+    }
+}
