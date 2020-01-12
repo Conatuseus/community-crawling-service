9dcc28333c0527604df36912232374d2998df855
MK S <conatuseus@gmail.com>
2020-01-12 18:15:22 +0900

    feature: Posts의 findById에 대한 테스트 코드 구현

--- src/test/java/com/conatuseus/communityservice/domain/posts/controller/PostsApiControllerTest.java
+++ src/test/java/com/conatuseus/communityservice/domain/posts/controller/PostsApiControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -27,6 +28,26 @@ public class PostsApiControllerTest {
         save(requestDto)
             .expectStatus().isCreated();
     }
+
+    @Test
+    void findById() {
+        //given
+        PostsSaveRequestDto requestDto = new PostsSaveRequestDto("title", "link", "okky", "성남");
+        EntityExchangeResult<byte[]> entityExchangeResult = save(requestDto)
+            .expectHeader().valueMatches("Location", PostsApiController.V1_POSTS + "/\\d*")
+            .expectBody().returnResult();
+
+        //when&then
+        webTestClient.get()
+            .uri(entityExchangeResult.getResponseHeaders().getLocation().toASCIIString())
+            .exchange()
+            .expectBody()
+            .jsonPath("$.id").isNotEmpty()
+            .jsonPath("$.title").isEqualTo(requestDto.getTitle())
+            .jsonPath("$.link").isEqualTo(requestDto.getLink())
+            .jsonPath("$.community").isEqualTo(requestDto.getCommunity());
+    }
+
 
     private WebTestClient.ResponseSpec save(final PostsSaveRequestDto requestDto) {
         return webTestClient.post()
