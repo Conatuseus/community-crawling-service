c63b5e1c1be5881a87426af55f8682e82f07505d
MK S <conatuseus@gmail.com>
2020-01-12 21:39:36 +0900

    feature: Community Update 메서드 구현

--- src/main/java/com/conatuseus/communityservice/domain/community/domain/Community.java
+++ src/main/java/com/conatuseus/communityservice/domain/community/domain/Community.java
@@ -1,5 +1,6 @@
 package com.conatuseus.communityservice.domain.community.domain;
 
+import com.conatuseus.communityservice.domain.community.service.dto.CommunityUpdateRequestDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,5 +36,14 @@ public class Community {
         this.keyword = keyword;
         this.cssQuery = cssQuery;
         this.attributeKey = attributeKey;
+    }
+
+    public Community update(final CommunityUpdateRequestDto requestDto) {
+        this.name = requestDto.getName();
+        this.searchUrl = requestDto.getSearchUrl();
+        this.keyword = requestDto.getKeyword();
+        this.cssQuery = requestDto.getCssQuery();
+        this.attributeKey = requestDto.getAttributeKey();
+        return this;
     }
 }
