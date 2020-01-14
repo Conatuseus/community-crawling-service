58eb7acb3ff6d350e7c9fc906495e9c832d7f8ac
MK S <conatuseus@gmail.com>
2020-01-14 22:41:51 +0900

    feature: 크롤링 스케줄 기능 추가

--- src/main/java/com/conatuseus/communityservice/CommunityCrawlingServiceApplication.java
+++ src/main/java/com/conatuseus/communityservice/CommunityCrawlingServiceApplication.java
@@ -2,7 +2,9 @@ package com.conatuseus.communityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CommunityCrawlingServiceApplication {
 
