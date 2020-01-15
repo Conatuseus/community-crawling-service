dc4cd9da4007bbb324528b09472c21010094b089
MK S <conatuseus@gmail.com>
2020-01-15 10:43:23 +0900

    refactor: config 패키지를 상위 패키지로 이동

--- src/main/java/com/conatuseus/communityservice/config/JpaConfig.java
+++ src/main/java/com/conatuseus/communityservice/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.conatuseus.communityservice.domain.config;
+package com.conatuseus.communityservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
