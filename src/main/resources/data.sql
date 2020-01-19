7fbd7d7bb2a5fb3cad3f28993777ae2b47b66513
MK S <conatuseus@gmail.com>
2020-01-19 23:16:19 +0900

    refactor: 컬럼 name 변경에 따른 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO community(id, name, base_url, search_url, keyword, css_query, attribute_key) VALUES (987654321, 'CLIEN', 'https://www.clien.net',
+INSERT INTO community(COMMUNITY_ID, name, base_url, search_url, keyword, css_query, attribute_key) VALUES (987654321, 'CLIEN', 'https://www.clien.net',
 'https://www.clien.net/service/search?q=%EC%95%84%EC%9D%B4%ED%8F%B0', '아이폰', 'span.list_subject > a', 'href');
 
-INSERT INTO community(id, name, base_url, search_url, keyword, css_query, attribute_key) VALUES (987654322, 'OKKY', 'https://okky.kr',
+INSERT INTO community(COMMUNITY_ID, name, base_url, search_url, keyword, css_query, attribute_key) VALUES (987654322, 'OKKY', 'https://okky.kr',
 'https://okky.kr/articles/community?query=%EC%84%B1%EB%82%A8&sort=id&order=desc', '성남', 'h5.list-group-item-heading.list-group-item-evaluate > a', 'href');
